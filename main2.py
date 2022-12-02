4e7bf1495d08bc1e2a5d0a548a93e35348bfe083
amir mohammad <amirmohammad@gamil.com>
2022-12-02 21:44:58 +0330

    Edit file main2.py

--- main2.py
+++ main2.py
@@ -1,4 +1,6 @@
 import sqlite3
+import time as tm
+
 
 con = sqlite3.connect("DB_mahdi_amir.db")
 cur = con.cursor()
@@ -25,13 +27,45 @@ class modify_database:
             "REFERENCES course(cid));")
         print("++ Table student_course created successfully")
 
+    def insert_data_into_table_student(self):
+        cur.execute("INSERT OR REPLACE INTO student(sid, name, code_meli) VALUES(100, 'Ali', 1250678912);")
+        con.commit()
+        print("++ Insert data into table_student successfully")
+
+    def display_data_into_table_student(self):
+        cur.execute("SELECT * FROM STUDENT;")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        print("++ Show data into table_student successfully")
+
+    def update_data_into_table_student(self):
+        cur.execute("UPDATE student set name = 'Mahdi' WHERE sid = 100;")
+        con.commit()
+        print("++ Update data into table_student successfully")
+
 
 # ---------------------------------------------------------------------------------------
 # create object from class
 db = modify_database()
 
 db.create_table_student()
+tm.sleep(1)
+
 db.create_table_course()
+tm.sleep(1)
+
 db.create_table_student_courses()
+tm.sleep(1)
+
+db.insert_data_into_table_student()
+tm.sleep(1)
+
+print("** Display student table information: ")
+db.display_data_into_table_student()
+tm.sleep(1)
+
+db.update_data_into_table_student()
+
 
 # todo : add data to tables & add some function for SQL commands like SELECT INSERT UPDATE and ...
