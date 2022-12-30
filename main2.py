2d2f3d4d869d2ef03749824e950699c95795f986
amir mohammad <amirmohammad@gamil.com>
2022-12-31 00:32:48 +0330

    Add insert, select, delete and fix procedure

--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ con = sqlite3.connect("DB_mahdi_amir.db")
 cur = con.cursor()
 
 
-class modify_database:
+class modify_database:        
     def create_table_student(self):
         cur.execute("CREATE TABLE if not exists student(sid int PRIMARY KEY ,name VARCHAR(50), code_meli int) ;")
         print("++ Table student created successfully")
@@ -45,9 +45,48 @@ class modify_database:
         print("++ Update data into table_student successfully")
 
 
+    def insert_data_into_table_course(self):
+        course_data = [
+                        [1000,'C++',2022],
+                        [2000,'PHP',2020],
+                        [3000,'Python',2023],
+                        [4000,'C#',2018],
+                        [5000,'ASP.NET',2020],
+                        [6000,'C',2010],
+                        [7000,'Ruby',2020]
+                    ]
+        insert_q = []
+
+        for cou_data in course_data:
+             cid = cou_data[0]
+             course_name = cou_data[1]
+             year = cou_data[2]
+             q=f"INSERT OR REPLACE INTO course VALUES ('{cid}','{course_name}','{year}');"
+             insert_q.append(q)
+
+        for q in insert_q:
+             cur.execute(q)
+
+        con.commit()
+        print("++ Insert data into table_course successfully")
+
+
+    def display_courses_in_table_course(self):
+        cur.execute("SELECT course_name FROM course;")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        print("++ Show courses in table_course successfully")
+
+
+    def delete_data_into_table_course(self):
+        cur.execute("DELETE FROM course WHERE year = 2020;")
+        con.commit()
+        print("++ delete data year = 2020 from table course successfully")
 
 
     #Procedure with INPUT Parameters
+    """
     def procedure_insert_data_into_table_student(self):
         cur.execute("CREATE PROCEDURE insertStudent @sid int, @name VARCHAR(50), @code_meli int
                     AS INSERT OR REPLACE INTO student(sid, name, code_meli) VALUES(@sid, @name, @code_meli);")
@@ -69,7 +108,7 @@ class modify_database:
                     AS SELECT @n=name FROM student WHERE sid=@id;")
         cur.execute("DECLARE @x char(10) EXEC maxsid 100, @x output print @x")
         con.commit()
-    
+    """
 
 
 
@@ -97,6 +136,19 @@ db.display_data_into_table_student()
 tm.sleep(1)
 
 db.update_data_into_table_student()
+tm.sleep(1)
+
+
+#insert data into table course
+db.insert_data_into_table_course()
+tm.sleep(1)
+
+print("** Display courses: ")
+db.display_courses_in_table_course()
+tm.sleep(1)
+
+db.delete_data_into_table_course()
+
 
 
 # todo : add data to tables & add some function for SQL commands like SELECT INSERT UPDATE and ...
